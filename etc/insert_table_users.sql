9f01c066d9a8c8da2890dc324920b1cc47e36e73
KAZUHIKO <kazuhiko.d@gmail.com>
2023-07-31 14:55:21 +0900

    テスト用ログインユーザーSQL

---
+++ etc/insert_table_users.sql
@@ -0,0 +1,16 @@
+insert into `users` (
+    `name`,
+    `email`,
+    `password`
+)
+values
+(
+    'テスト花子',
+    'test_hanako@example.com',
+    'test_hanako'
+),
+(
+    'テスト太郎',
+    'test_tarou@example.com',
+    'test_tarou'
+);
