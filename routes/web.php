4174c760b4fc627f48761242857d200ce4f3bc7c
KAZUHIKO <kazuhiko.d@gmail.com>
2023-08-01 13:16:05 +0900

    a

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/login', function () {
+    return view('index');
 });
 
 Route::get('/dashboard', function () {
