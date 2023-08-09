87348c2b4cc0cde0833aede3e85301ab2d9733ad
KAZUHIKO <kazuhiko.d@gmail.com>
2023-08-09 14:58:56 +0900

    コントローラー作成

---
+++ app/Http/Controllers/Todo_itemController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo_item;
+use Illuminate\Http\Request;
+
+class Todo_itemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Todo_item $todo_item)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Todo_item $todo_item)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Todo_item $todo_item)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Todo_item $todo_item)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\Todo_itemController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return view('login/index');
 });
+
+Route::resource('todo_item', Todo_itemController::class);
+// Route::post('todo/{todo}/delete', [Todo_itemController::class, 'delete'])->name('todo.delete');
+// Route::patch('todo/{todo}/complete', [Todo_itemController::class, 'complete'])->name('todo.complete');
+
+Route::resource('login', LoginController::class);
