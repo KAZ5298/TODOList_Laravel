72a8604d713355162197a38e8a8ba51e1f758646
KAZ5298 <kazuhiko.d@gmail.com>
2023-08-09 20:19:00 +0900

    初期画面をログイン画面に変更

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,67 +1,47 @@
-<!DOCTYPE html>
-<html lang="ja">
+<x-guest-layout>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta http-equiv="content-type" content="text/html; charset=utf-8">
-    <title>ログイン</title>
-    {{-- <link rel="stylesheet" href="../css/bootstrap.min.css"> --}}
-    <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
-</head>
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
 
-<body>
-    <nav class="navbar navbar-expand-md navbar-dark bg-primary">
-        <span class="navbar-brand">TODOリスト</span>
-    </nav>
-
-    <div class="container">
-        <div class="row my-2">
-            <div class="col-sm-3"></div>
-            <div class="col-sm-3">
-                <h1></h1>
-            </div>
-            <div class="col-sm-3"></div>
+        <!-- Email Address -->
+        <div>
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="row my-2">
-            <div class="col-sm-3"></div>
-            <div class="col-sm-6 alert alert-danger alert-dismissble fade show">
-                ユーザー名またはパスワードが違います。 <button class="close" data-dismiss="alert">&times;</button>
-            </div>
-            <div class="col-sm-3"></div>
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <div class="row my-2">
-            <div class="col-sm-3"></div>
-            <div class="col-sm-6">
-                <form action="{{ route('login') }}" method="post">
-                    @csrf
-                    <div class="form-group">
-                        <label for="user">ユーザー名</label>
-                        {{-- <input type="text" class="form-control" id="user" name="user"> --}}
-                        <input type="text" class="form-control" id="email" name="email">
-                    </div>
-                    <div class="form-group">
-                        <label for="password">パスワード</label>
-                        <input type="password" class="form-control" id="password" name="password">
-                    </div>
-                    <button type="submit" class="btn btn-primary">ログイン</button>
-                </form>
-
-            </div>
-            <div class="col-sm-3"></div>
+        <!-- Remember Me -->
+        <div class="block mt-4">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            </label>
         </div>
 
-    </div>
+        <div class="flex items-center justify-end mt-4">
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
 
-    <!-- 必要なJavascriptを読み込む -->
-    {{-- <script src="../js/jquery-3.4.1.min.js"></script>
-    <script src="../js/bootstrap.bundle.min.js"></script> --}}
-    <script src="{{ asset('/js/jquery-3.4.1.min.js"') }}"></script>
-    <script src="{{ asset('/js/bootstrap.bundle.min.js') }}"></script>
-
-</body>
-
-</html>
+            <x-primary-button class="ml-3">
+                {{ __('Log in') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
